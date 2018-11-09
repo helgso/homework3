f69f2538d5b4a266504a8b6e1404c3451a888dec
jtrudel <jeremy_trudel@hotmail.com>
2018-11-09 01:16:11 -0500

    bprop fini

--- oursolution/neuralnetwork.py
+++ oursolution/neuralnetwork.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 # Set current working directory as this file's path
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
@@ -13,27 +15,13 @@ import numpy as np
 def init_weights(shape):
     return np.random.uniform(-1/np.sqrt(shape[0]), 1/np.sqrt(shape[0]), shape)
 
-
 def softmax(x, axis=0):
     a = np.exp(x - np.amax(x, axis=axis, keepdims=True))
     return a / np.sum(a, axis=axis, keepdims=True)
 
-def softmax_prime(output_activations, y):
-    """
-    Derivative of the softmax function
-
-    :param delta: The change to the output activations we want
-    :param y: The output label
-    :return: dL/do^a
-    """
-    return output_activations - onehot(y, len(output_activations))
-
-
-def onehot(y, m):
-    oh = np.zeros(m)
-    oh[y] = 1
-    return oh
-
+def softmax_prime(a, y, axis=0):
+    """Derivative of the softmax function."""
+    return  
 
 def relu(z):
     """
@@ -43,42 +31,28 @@ def relu(z):
     """
     return np.maximum(z, 0)
 
-
 def relu_prime(z):
     return np.greater(z, 0)
 
-
 def log_loss(o_y):
     return -np.log(o_y)
-
 
 def gradient_approx(x, eps=None):
     if eps is None or eps < 10 ** -6 or eps > 10 ** -4:
         eps = np.random.uniform(10 ** -6, 10 ** -4)
     return (log_loss(x+eps) - log_loss(x-eps)) / (2 * eps)
 
-
 def main():
     # Circles.txt training data
-    circles_raw = np.loadtxt('data/circles/circles.txt')
-    circles_training_data = [[x, int(y)] for x, y in zip(circles_raw[:, :-1], circles_raw[:, -1])]
-
-    helgi_circles_test = [circles_training_data[0]]
+    circles_data = np.loadtxt('data/circles/circles.txt')
 
     # MNIST training data
-    # x_mnist_raw_train, y_mnist_raw_train = mnist_reader.load_mnist('data/fashion', kind='train')
-    # x_mnist_raw_test, y_mnist_raw_test = mnist_reader.load_mnist('data/fashion', kind='t10k')
-    # mnist_training_set = [[x, y] for x, y in (x_mnist_raw_train, y_mnist_raw_train)]
-    # mnist_test_set = [[x, y] for x, y in (x_mnist_raw_test, y_mnist_raw_test)]
+    x_train, y_train = mnist_reader.load_mnist('data/fashion', kind='train')
+    x_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k')
 
-    circles_network = Network([2, 2, 2])
-    circles_network.train(
-        training_data=circles_training_data,
-        epochs=1,
-        mini_batch_size=10,
-        step_size=1
-    )
-
+    # our_algorithm()
+    model = Network([784,300,10])
+    model.train(x_train,
 
 class Network(object):
     def __init__(
@@ -92,8 +66,10 @@ class Network(object):
         """
         self.num_layers = len(num_neurons)
         self.layer_sizes = num_neurons
-        self.b = [np.zeros(layer_size) for layer_size in self.layer_sizes[1:]]
-        self.w = [init_weights(size) for size in zip(self.layer_sizes[:-1], self.layer_sizes[1:])]
+        self.b = [np.zeros((layer_size, 1)) for layer_size in self.layer_sizes[1:]]
+        self.w = [
+            init_weights(size) for size in zip(self.layer_sizes[:-1], self.layer_sizes[1:])
+        ]
 
     def finite_difference_gradient_check(
         self,
@@ -162,77 +138,53 @@ class Network(object):
         # TODO: Define all the gradients and update our weights and biases
         # Copy-paste from network2.py (lmbda regularization term removed.
         # We need to add our elastic-net regularization)
-        gradient_b = [np.zeros(b.shape) for b in self.b]
-        gradient_w = [np.zeros(w.shape) for w in self.w]
+        nabla_b = [np.zeros(b.shape) for b in self.b]
 
         for x, y in mini_batch:
-            delta_gradient_b, delta_gradient_w = self.bprop(x, y)
-            gradient_b = [nb+dnb for nb, dnb in zip(gradient_b, delta_gradient_b)]
-            gradient_w = [nw+dnw for nw, dnw in zip(gradient_w, delta_gradient_w)]
+            o, L = self.fprop(x,y)
+            delta_nabla_b, delta_nabla_w = self.bprop(o, y)
 
-        self.w = [w - (step_size / len(mini_batch)) * nw for w, nw in zip(self.w, gradient_w)]
-        self.b = [b - (step_size / len(mini_batch)) * nb for b, nb in zip(self.b, gradient_b)]
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+
+        self.w = [w - (step_size / len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)]
+        self.b = [b - (step_size / len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)]
 
     def fprop(self, x, y):
-        self.zs = []
+        self.zs = [x]
         z = x
-        for w, b, a in zip(self.w, self.b):
-            z = relu(np.dot(w.T, z) + b)
+        for w, b in zip(self.w, self.b):
+            print(b.shape)
+            z = relu(w.T @ z + b)
             self.zs.append(z)
        
-        o = softmax(a)
+        o = softmax(z)
         L = log_loss(o[y])
         return o, L
-
-    def bprop(
-        self,
-        output,
-        y
-    ):
+            
+    def bprop(self, output, y):
         """
-        Returns gradient_b, gradient_w representing the gradients for the cost function
+        Returns a tuple (nabla_b, nabla_w) representing the gradient for the cost function C_x
         """
-        gradient_b = [np.zeros(b.shape) for b in self.b]
-        gradient_w = [np.zeros(w.shape) for w in self.w]
+        nabla_b = [np.zeros(b.shape) for b in self.b]
+        nabla_w = [np.zeros(w.shape) for w in self.w]
 
-        # forward pass
-        activation = x
-        activations = [x]  # list to store all the activations, layer by layer
-        zs = []  # list to store all the z vectors, layer by layer
-        iterations = self.num_layers-1
-        for i in range(iterations):
-            z = np.dot(self.w[i], activation) + self.b[i]
-            zs.append(z)
+        grad_output = output - self.onehot(y)
+        e_prev = (grad_output * relu_prime(self.zs[-1]))
+        nabla_w[-1] = relu(self.zs[-2]) @ e_prev.T
 
-            if i < iterations-1:
-                # Every intermediate layer uses a relu activation
-                activation = relu(z)
-            else:
-                # The output layer uses a softmax activation
-                activation = softmax(z)
+        for k in reversed(range(len(self.w[:-1]))):
+            e_now = self.w[k+1] @ e_prev * relu_prime(self.zs[k+1])
+            nabla_w[k] = e_now @ self.zs[k].T
+            nabla_b[k] = e_now
+            e_prev = e_now
 
-            activations.append(activation)
+        return nabla_b, nabla_w
 
-        # backward pass
-        dL_do_a = softmax_prime(activations[-1], y)
-
-        last_hidden_size = self.layer_sizes[-2]
-        output_size = self.layer_sizes[-1]
-
-        gradient_b[-1] = dL_do_a
-        gradient_w[-1] = np.array([
-            [dL_do_a[p] * activations[-2][k] for p in range(0, output_size)] for k in range(0, last_hidden_size)
-        ])
-
-        for l in range(2, self.num_layers):
-            dodL_dhdo = np.dot(self.w[-l+1].transpose(), dL_do_a)
-
-            gradient_b[-l] = dodL_dhdo
-            gradient_w[-l] = np.array([
-                [activations[-l-1][j] * sum([self.w[-l+1][k] * dL_do_a[p] for p in range(0, output_size)]) for k in range(0, last_hidden_size)] for j in range(self.layer_sizes[-l-1])
-            ])
-
-        return gradient_b, gradient_w
+    def onehot(self, y):
+        oh = np.zeros((self.layer_sizes[-1],1)).astype(int)
+        oh[y-1] = 1
+        return oh
 
 
 if __name__ == '__main__':
